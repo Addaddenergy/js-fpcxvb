544a0f94a6d8fc4ea8f641f86e7158296d1c0a1e
Addaddenergy <addaddenergy@gmail.com>
2021-12-23 11:49:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fpcxvb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fpcxvb)
---
+++ index.js
@@ -0,0 +1,17 @@
+/<Placemark id=”1">
+<Polygon>
+  <outerBoundaryIs>
+    <LinearRing>
+      <coordinates> 
+        152.84486,-26.758348 152.84604,-26.758348 152.84604,-26.757237 152.84486,-26.757237 152.84486,-26.758348 
+      </coordinates>
+    </LinearRing>
+  </outerBoundaryIs>
+</Polygon>
+</Placemark>
+/ Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
